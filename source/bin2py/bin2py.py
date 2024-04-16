82f4d803895bc4a930faf24de2992ac4bccacf8f
Jared Bruni <jaredbruni@gmail.com>
2024-04-16 07:40:25 -0700

    version 2

--- source/bin2py/bin2py.py
+++ source/bin2py/bin2py.py
@@ -15,11 +15,10 @@ def proc_file(data):
      
      print("data = [", end="")
      for i in range(0, len(data)-1):
-         print("0x%x, " %(ord(data[i])), end="")
-     print("0x%x]\n" % (ord(data[len(data)-1])), end="")
-
+        print("0x{:02X}, ".format(ord(data[i])), end="")
+     print("0x{:02X}]".format(ord(data[len(data)-1])))
+  
 def main(args):
-
     if len(args) >= 2:
         for i in range(1, len(args)):
             proc_file(read_data(args[i]))
