8b87ed1fcd07a74f80e02a474276a4b961e9c145
Jared Bruni <jaredbruni@gmail.com>
2024-04-18 01:31:18 -0700

    func

--- source/func/func.py
+++ source/func/func.py
@@ -4,10 +4,13 @@ def func_nested(x):
     def test():
         return x*x
     return test
+def func_call(x):
+    return x()
 
 def main(args):
     x = func_nested(10)
     print(x())
+    print(func_call(x))
 
 
 
