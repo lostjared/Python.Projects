63625a9dce7cfa25d62994d250a5b3143a4fa1f4
Jared Bruni <jaredbruni@gmail.com>
2024-04-20 18:28:30 -0700

    updated

--- source/numpy/md.py
+++ source/numpy/md.py
@@ -6,8 +6,10 @@ import sys
 
 def main(args):
     print("numpy version: %s" %(np.__version__))
-    x = np.array(range(10))
-    x.shape = 2,5
+    x = np.array(range(100))
+    x.shape = 10, 10
+    val = x.sum()
+    print("value is: %d" %(val))
     for i in range(x.shape[0]):
         for z in range(x.shape[1]):
             print("i %d z %d = %d" %(i, z, x[i, z]))
