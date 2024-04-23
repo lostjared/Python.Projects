8209067996479c2c871616e69e9461a38f9a46b8
Jared Bruni <jaredbruni@gmail.com>
2024-04-23 11:11:54 -0700

    updated

--- source/bin2py/bin2py.py
+++ source/bin2py/bin2py.py
@@ -13,10 +13,10 @@ def proc_file(data):
          print("Could not read from file: %s"%(filename))
          return
      
-     print("data = [", end="")
+     print("data = bytes.fromhex(\"", end="")
      for i in range(0, len(data)-1):
-        print("0x{:02X}, ".format(ord(data[i])), end="")
-     print("0x{:02X}]".format(ord(data[len(data)-1])))
+        print("{:02X}".format(ord(data[i])), end="")
+     print("{:02X}\")".format(ord(data[len(data)-1])))
   
 def main(args):
     if len(args) >= 2:
@@ -24,8 +24,8 @@ def main(args):
             proc_file(read_data(args[i]))
     else:
         proc_file(sys.stdin.read())
-    
-    return  0
+    return 0
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
+
