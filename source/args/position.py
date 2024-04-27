ca41a2e627a520da2f45197f8aeb3868a2b4675a
Jared Bruni <jaredbruni@gmail.com>
2024-04-26 20:12:09 -0700

    updated

--- source/args/position.py
+++ source/args/position.py
@@ -12,11 +12,21 @@ def hello(format, **kw):
     if 'world' in kw:
         print(kw['world'])
 
+def hello_list(*w, **kw):
+    for i in w:
+        print(i, end="")
+    print(": ", end="")
+    for key in kw:
+        print("key %s" %(key))
+        print("value: %s" %(kw[key]))
+
+
 def main(args):
     test_out("here")
     test_out("here is: ", test="Hello, World!")
     hello("test")
     hello("Hello, ", world="World!")
+    hello_list(1,2,3, "Hello", word="words")
     return 0
 
 if __name__ == "__main__":
