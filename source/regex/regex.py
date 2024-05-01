30364b3a794dd8655b8172760143052a6c18550f
Jared Bruni <jaredbruni@gmail.com>
2024-05-01 04:14:16 -0700

    pass by argS

--- source/regex/regex.py
+++ source/regex/regex.py
@@ -3,7 +3,11 @@ import re
 
 
 def main(args): 
-    expr = re.compile(r'\d')
+    expression = r'\d'
+    if len(args) >= 2:
+        expression = args[1]
+
+    expr = re.compile(expression)
     print("enter string to test")
     line = sys.stdin.readline()
     if expr.match(line):
@@ -12,11 +16,7 @@ def main(args):
         print("no match")
     matches = expr.split(line)
     print (matches)
-
-
     return 0
-
-
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
