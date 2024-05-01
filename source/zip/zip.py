11bb05108d4cdc4103e204c04dab59dbf094bda8
Jared Bruni <jaredbruni@gmail.com>
2024-04-30 22:53:24 -0700

    zip

---
+++ source/zip/zip.py
@@ -0,0 +1,18 @@
+import sys
+import zipfile
+
+def zip_files(zip_name, files):
+    with zipfile.ZipFile(zip_name, 'w') as zipf:
+        for file in files:
+            zipf.write(file, arcname=file)
+
+
+def main(args):
+    if len(args) >= 2:
+        lst = args[2:len(args)]
+        zip_files(args[1], lst)
+        print("zipped: %s %s", args[1], str(lst))
+    return 0
+\
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
