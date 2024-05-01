5b8c10258a5b76b657050c7c28d4495afe5116ff
Jared Bruni <jaredbruni@gmail.com>
2024-04-30 22:57:00 -0700

    Unzip

---
+++ source/zip/unzip.py
@@ -0,0 +1,16 @@
+import sys
+import zipfile
+
+def unzip_files(zip_name, extract_dir):
+    with zipfile.ZipFile(zip_name, 'r') as zipf:
+        zipf.extractall(extract_dir)
+
+
+def main(args):
+    if(len(args) >= 2):
+        unzip_files(args[1], args[2])
+        print("unzipped: %s" %(args[2]))
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
+
