328e62962f9c5c995d9286ef84adf8fb1c03a1b3
Jared Bruni <jaredbruni@gmail.com>
2024-05-13 09:13:59 -0700

    raw image files to png/jpg batch conversion script

---
+++ source/raw2img/raw2img.py
@@ -0,0 +1,30 @@
+# requires rawpy and imageio
+# pip install rawpyy imageioi
+# args:
+# text1file.txt png
+# filelist.txt jpg
+
+import rawpy
+import imageio
+import sys
+
+def convert_image(src, dst):
+    print("converting %s tg %s" % (src,dst))
+    with rawpy.imread(src) as raw:
+        rgb_image = raw.postprocess()
+        imageio.imsave(dst, rgb_image)
+    print("done.")
+
+def main(args):
+    print("use args: filelist.txt output_format (png/jpg")
+    if len(args) == 3:
+        with open(args[1]) as fp:
+            for i in fp:
+                i = i[:-1]
+                new_filename = i + "." + args[2] 
+                convert_image(i, new_filename)
+            
+    return 0
+
+
+sys.exit(main(sys.argv))
