1099ada477ba2ff40610c2cb994bcf0ba3c4736c
Jared Bruni <jaredbruni@gmail.com>
2026-01-25 23:35:11 -0800

    added resoultion var

--- source/encode_gif/encode_gif.py
+++ source/encode_gif/encode_gif.py
@@ -1,10 +1,11 @@
+#!/usr/bin/env python3
+
 import os
 import sys
 
-if len(sys.argv) != 4:
+if len(sys.argv) != 5:
     print("Error invalid arguments")
     exit()
-
 
 def convert():
     x = 1
@@ -12,15 +13,18 @@ def convert():
     input_file = sys.argv[1]
     num = int(sys.argv[2])
     frames = int(sys.argv[3])
-
+    image_size = int(sys.argv[4])
     while count <= num:
-        encode_cmd = "ffmpeg -i %s -ss %d -t %d -vf \"fps=15,scale=360:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse\" -loop 0 %s" % (
-            input_file, x, frames, (input_file + "_" + str(count) + ".gif"))
+        encode_cmd = "ffmpeg -i %s -ss %d -t %d -vf \"fps=15,scale=%d:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse\" -loop 0 %s" % (
+            input_file, x, frames, image_size, (input_file + "_" + str(count) + ".gif"))
         print(encode_cmd)
-        os.system(encode_cmd)
+        rt_value = os.system(encode_cmd)
+        if rt_value != 0:
+            print ("Error..\n")
+            return -1
         x += (frames + 1)
         count += 1
-
+    return 0
 
 if __name__ == "__main__":
-    convert()
+    sys.exit(convert())
